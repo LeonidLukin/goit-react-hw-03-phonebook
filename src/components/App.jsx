0e77e3085ccaaa160942e8614e1a4e3930f08051
LeonidLukin <leonidlukin90@gmail.com>
2022-09-27 20:31:22 +0300

    used lifecycle

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,17 +1,37 @@
 import { Component } from 'react';
 import { nanoid } from 'nanoid';
 import { Report } from 'notiflix/build/notiflix-report-aio';
+import { BsFillPersonPlusFill } from 'react-icons/bs';
 import ContactForm from './ContactForm';
 import ContactList from './ContactList';
 import Filter from './Filter/Filter';
 import Message from './Message';
+import Modal from './Modal'
 import css from './App.module.css';
 
 class App extends Component {
   state = {
     contacts: [],
     filter: '',
+    showModal: false,
   };
+
+  componentDidMount() {
+    const contacts = localStorage.getItem('contacts')
+    const parsedContacts = JSON.parse(contacts);
+
+    if (parsedContacts) {
+      this.setState({ contacts: parsedContacts })
+    }
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (this.state.contacts !== prevState.contacts) {
+      console.log('Обновилось поле контакты');
+
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts))
+    }
+  }
 
   addContact = ({ name, number }) => {
     const { contacts } = this.state;
@@ -46,20 +66,33 @@ class App extends Component {
     );
   };
 
+  toggleModal = () => {
+    this.setState(({ showModal }) => ({ showModal: !showModal }))
+  }
+
   render() {
-    const { filter } = this.state;
+    const { filter, showModal } = this.state;
     const addContact = this.addContact;
     const changeFilter = this.changeFilter;
     const filtredContacts = this.filtredContacts();
     const deleteContact = this.deleteContact;
     const length = this.state.contacts.length;
+    const toggleModal = this.toggleModal;
 
     return (
       <div className={css.container}>
         <h1 className={css.title}>
           Phone<span className={css.title__color}>book</span>
         </h1>
-        <ContactForm onSubmit={addContact} />
+        <button className={css.button} type="button" onClick={toggleModal}>
+          <span className={css.button__text}>Add new contact</span>{' '}
+          <BsFillPersonPlusFill size={20} />
+        </button>
+        {showModal && (
+          <Modal onClose={toggleModal} title="add contact">
+            <ContactForm onSubmit={addContact} />
+          </Modal>
+        )}
 
         <h2 className={css.subtitle}>Contacts</h2>
         <Filter filter={filter} changeFilter={changeFilter} />
